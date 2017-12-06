e49d5e8bc02355a47677bf73cbb699b7b4f83e4b
JP Sheehan <jsheehan@world-vu.net>
2017-12-06 15:31:13 -0500

    remove old .m files

--- currentScenario.m
+++
@@ -1,8 +0,0 @@
-function [root, scenario] = currentScenario()
-
-app = actxGetRunningServer('STK11.application');
-
-% Grab a handle on the STK application root.
-root = app.Personality2;
-
-scenario = root.CurrentScenario;
