a1a24e8451cd169ce1b9d3d07b1e9b908b6500ea
JP Sheehan <jsheehan@world-vu.net>
2017-12-06 16:59:46 -0500

    removed tracking sensor

--- ow_stk/sensor.py
+++ ow_stk/sensor.py
@@ -46,31 +46,3 @@ def addFixed(obj, name, shape, dim1, dim2, az = 0, el = 90):
 
     return sensors
 
-def addTracking(obj, name, shape, dim1, dim2, target):
-
-    pass
-    #  sensor = obj.Children.New(STKObjects.eSensor, name)
-    #  sensor2 = sensor.QueryInterface(STKObjects.IAgSensor)
-    #
-    #  # set pointing
-    #  sensor2.SetPointingType(STKObjects.)
-    #  sensor2.CommonTasks.SetPointingFixedAzEl(az, el, STKObjects.eBoresightHold)
-    #
-    #  # set beam shape
-    #  if shape.lower().strip() == 'elipse':
-    #
-    #      sensor2.SetPatternType(STKObjects.eSnComplexConic)
-    #      minAngle = 0
-    #      maxAngle = 360
-    #      sensor2.CommonTasks.SetPatternComplexConic(dim1, dim2, minAngle, maxAngle)
-    #
-    #  elif shape.lower().strip() == 'rectangle':
-    #
-    #      sensor2.SetPatternType(STKObjects.eSnRectangular)
-    #      sensor2.CommonTasks.SetPatternRectangular(dim1, dim2)
-    #
-    #  else:
-    #
-    #      raise ValueError('Unsupported sensor pattern')
-    #
-    #  return sensor
