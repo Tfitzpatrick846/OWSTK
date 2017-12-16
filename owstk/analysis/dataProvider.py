38fe1d0ceee04e4523b595fbc2ba240c62c56ab9
JP Sheehan <jsheehan@world-vu.net>
2017-12-16 16:04:00 -0500

    Call function to get values

--- owstk/analysis/dataProvider.py
+++ owstk/analysis/dataProvider.py
@@ -1,24 +1,24 @@
-def timeVarData(obj, dataPath, dataName, startTime, stopTime, dt=60):
+def timeVar(obj, dataPath, dataName, startTime, stopTime, dt=60):
 
     dataProvider = obj.DataProviders \
         .GetDataPrvTimeVarFromPath(dataPath) \
         .Exec(startTime, stopTime, dt)
     # could potentially be sped up by using ExecElements
 
-    return dataProvider.DataSets.GetDataSetByName(dataName).GetValues
+    return dataProvider.DataSets.GetDataSetByName(dataName).GetValues()
 
-def fixedData(obj, dataPath, dataName):
+def fixed(obj, dataPath, dataName):
     
     dataProvider = obj.DataProviders \
         .GetDataPrvFixedFromPath(dataPath) \
         .Exec()
 
-    return dataProvider.DataSets.GetDataSetByName(dataName).GetValues
+    return dataProvider.DataSets.GetDataSetByName(dataName).GetValues()
 
-def intervalData(obj, dataPath, dataName, startTime, stopTime):
+def interval(obj, dataPath, dataName, startTime, stopTime):
     
     dataProvider = obj.DataProviders \
         .GetDataPrvIntervalFromPath(dataPath) \
         .Exec(startTime, stopTime)
 
-    return dataProvider.DataSets.GetDataSetByName(dataName).GetValues
+    return dataProvider.DataSets.GetDataSetByName(dataName).GetValues()
