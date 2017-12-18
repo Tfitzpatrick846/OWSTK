5d3d131eb166982a74d4e2a3b224af715848c4b3
JP Sheehan <jsheehan@world-vu.net>
2017-12-18 17:01:24 -0500

    scenario setup for testing

---
+++ testing.py
@@ -0,0 +1,7 @@
+import owstk
+
+sc, root, app = owstk.stk.new()
+
+satellites = owstk.oneweb.gen1.addSS3Constellation(sc, [0, 410])
+facilities = owstk.oneweb.gen1.addSNPs(sc, [4, 10])
+sensors = owstk.oneweb.gen1.attachUserAntennas(satellites[0])
