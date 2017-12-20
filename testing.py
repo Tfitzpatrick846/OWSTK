97f650b57d10a89bb41bcab4fb85f98296d10a38
JP Sheehan <jsheehan@world-vu.net>
2017-12-20 16:29:44 -0500

    Updated testing scenario

--- testing.py
+++ testing.py
@@ -3,6 +3,7 @@ import owstk
 #  sc, root, app = owstk.stk.new()
 sc, root, app = owstk.stk.current()
 
-satellites = owstk.oneweb.gen1.addSS3Constellation(sc, [0, 410])
+satellites = owstk.oneweb.gen1.addSS3Constellation(sc, [0, 1, 410])
 facilities = owstk.oneweb.gen1.addSNPs(sc, [4, 10])
 sensors = owstk.oneweb.gen1.attachUserAntennas(satellites[0])
+sensors = owstk.oneweb.gen1.attachUserAntennas(satellites[1])
