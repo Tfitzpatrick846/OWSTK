b35aa6b087a12c67e6ed6754333c2945df7d7879
JP Sheehan <jsheehan@world-vu.net>
2017-12-24 14:20:06 -0600

    Fixed eccentricity data path

--- owstk/analysis/orbit.py
+++ owstk/analysis/orbit.py
@@ -3,6 +3,6 @@ from . import dataProvider as dp
 def ecc(sat, startTime=None, stopTime=None, dt=60):
     """Get eccentricity array from satellite over given time"""
 
-    dataPath = 'CalculateKeplOrbit/Orbit'
+    dataPath = 'Parameter Set: Orbit/Orbit'
     dataName = 'Classical.Eccentricity'
     return dp.timeVar(sat, dataPath, dataName, startTime, stopTime, dt)
