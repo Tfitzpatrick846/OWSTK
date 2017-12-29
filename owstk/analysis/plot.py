72fc9e8de68677c90eaf5011442491fa3d072d50
JP Sheehan <jsheehan@world-vu.net>
2017-12-29 10:21:15 -0500

    more readable axis labels

--- owstk/analysis/plot.py
+++ owstk/analysis/plot.py
@@ -1,12 +1,17 @@
 """Plot results"""
 
 from matplotlib import pyplot as plt
+from matplotlib.dates import date2num
 import datetime
 
 def timePlot(time, values):
 
     pyplotdate = _datetime2pyplotdate(time)
-    plt.plot_date(pyplotdate, values)
+    fig, ax = plt.subplots()
+    plt.plot_date(pyplotdate, values, '-')
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(45)
+    plt.tight_layout()
     plt.show()
 
 def intervals(intrvls):
@@ -27,30 +32,27 @@ def intervals(intrvls):
         times.append(intrvl[1])
         val.append(0)
     
+    fig, ax = plt.subplots()
     pyplotdate = _datetime2pyplotdate(times)
     plt.plot_date(pyplotdate, val, '-')
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(45)
+    plt.tight_layout()
     plt.show()
 
 def _datetime2pyplotdate(time):
     """Convert datetime value to something useable by pyplot.plot_date"""
 
-    refday = datetime.datetime(1,1,1)
-    
     if (type(time) is list) or (type(time) is tuple):
 
         pyplotdate = []
 
         for k in range(len(time)):
             
-            dt = time[k] - refday
-            sec = dt.total_seconds()
-            days = sec / (60*60*24)
-            pyplotdate.append(days)
+            pyplotdate.append(date2num(time[k]))
+
     else:
 
-        dt = time - refday
-        sec = dt.total_second()
-        days = sec / (60*60*24)
-        pyplotdate = days
+        pyplotdate = date2num(time[k])
 
     return pyplotdate
