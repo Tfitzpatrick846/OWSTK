e9dd71b1bd0331d0f4c6bb9a27fcae1df0bd8cc7
JP Sheehan <jsheehan@world-vu.net>
2017-12-29 09:35:46 -0500

    compute access intervals

--- owstk/analysis/access.py
+++ owstk/analysis/access.py
@@ -2,7 +2,7 @@
 
 import comtypes
 from comtypes.gen import STKUtil, STKObjects
-
+from ..stk import str2datetime, datetime2str
 
 def compute(fromObj, toObj):
     """Compute the access from one object to another object."""
@@ -10,3 +10,14 @@ def compute(fromObj, toObj):
     access = fromObj.GetAccessToObject(toObj)
     access.ComputeAccess()
     return access
+
+def intervals(access):
+    """Compute access intervals and return as datetime pairs."""
+
+    agIntervals = access.ComputedAccessIntervalTimes
+    strIntervals = agIntervals.ToArray(0,-1)
+    intervals = []
+    for strInterval in strIntervals:
+        intervals.append(str2datetime(strInterval))
+
+    return tuple(intervals)
